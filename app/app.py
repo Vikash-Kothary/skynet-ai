e6094da3c6806fbd5259144fc36352db578147de
Ivan Savin <acccko@gmail.com>
2018-11-10 13:38:28 +0000

    socker run fixes

--- app/app.py
+++ app/app.py
@@ -10,16 +10,5 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-
-@app.route('/send_message/<text>', methods=['GET', 'POST'])
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
 if __name__ == '__main__':
     pass
--- app/sockets.py
+++ app/sockets.py
@@ -5,13 +5,18 @@ sockets.py - socketsio
 
 
 from flask_socketio import SocketIO, emit
+from app import app
 
 socketio = SocketIO(app)
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
-@sockets.route('/hello')
-def hello():
-    return 'Hello'
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 
 if __name__ == "__main__":
